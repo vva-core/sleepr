6c9775fed630c7e3cfe910a8f189c5622dd3afee
vva-core <vvitkovskyi4@gmail.com>
2026-03-20 15:36:31 +0200

    feat: add postgres to docker-compose

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -3,9 +3,17 @@ import { ReservationsService } from './reservations.service';
 import { ReservationsController } from './reservations.controller';
 import { ReservationsRepository } from './reservations.repository';
 import { DatabaseModule, LoggerModule } from '@app/common';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [DatabaseModule, LoggerModule],
+  imports: [
+    DatabaseModule,
+    LoggerModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env.local'],
+    }),
+  ],
   controllers: [ReservationsController],
   providers: [ReservationsService, ReservationsRepository],
 })
--- libs/common/src/prisma/prisma.service.ts
+++ libs/common/src/prisma/prisma.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from '../../../../generated/prisma';
+import { PrismaClient } from './generated/prisma';
 
 @Injectable()
 export class PrismaService
