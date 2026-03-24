8703521f24f3bb8a73a96576230a081df0cb4aec
vva-core <vvitkovskyi4@gmail.com>
2026-03-24 15:16:34 +0200

    feat: add pgadmin

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -2,10 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AuthModule } from './auth.module';
 import { ConfigService } from '@nestjs/config';
 import { Logger } from 'nestjs-pino';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
   const configService = app.get(ConfigService);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
   app.useLogger(app.get(Logger));
   await app.listen(configService.getOrThrow('PORT'));
 }
